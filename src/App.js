04f04e1d3a11e7a96e533936671e95d76e0aa419
Mxolisi <mxomag074@gmail.com>
2023-02-03 14:39:53 +0200

    Added skills section in Header component and updated navigation bar component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () =>
 {
     return (
     <div className="App">
-        <div>
+        <div className="appBar">
             <NavBar/>
             <Header/>
             
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,41 @@ import "./Header.css"
 import  profile  from "../../assets/profile.png";
 
 const Header = () => {
+  const csharp = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+  const java = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
+  const python = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+  const javascript ="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+  const css ="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+  const html ="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+  const react ="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+          
   return (
     <div className="Header">
       
-      <img src={profile} alt="profile"/>
+
       <div className="Profile">
-          <h3 data-testid ="name">Mxolisi Magubane</h3>
-          <p data-testid ="bio_description">Hello, World!</p> 
+          <img src={profile} alt="profile"/>
+          <div className="Profile_text">
+            <h3 data-testid ="name">Mxolisi Magubane</h3>
+            <p data-testid ="bio_description">Hello, World!</p>
+          </div>
+         
+           
+      </div>
+      <h2>Skills</h2>
+      <hr className="Line"
+     
+      />
+      <div className="skills_icons">
+         
+            <img src={csharp}  alt="csharp"/>
+            <img src={java}  alt="java"/>
+            <img src={python} alt="python"/>
+            <img src = {html} alt="html"/>
+            <img src={css} alt="css"/>
+            <img src= {javascript} alt="javascript"/> 
+            <img src= {react} alt ="react"/>
+          
       </div>
       
     </div>
